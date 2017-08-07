2cf4608c5fe6b1ce28fcaca566d3bf167603e547
test <test@corelogic.com>
2017-08-07 18:11:55 -0500

    AppModule clean build task added genrated file names to  gitignore

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by pivotal on 8/7/17.
+ */
+
+import  {NgModule} from '@angular/core';
+
+
+@NgModule({
+    imports: [NgModule],
+    declarations: [AppModule],
+    bootstrap: [AppModule]
+})
+export class AppModule {
+}
--- src/main-aot.ts
+++ src/main-aot.ts
@@ -3,4 +3,5 @@ import { platformBrowser }    from '@angular/platform-browser';
 import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
 
 console.log('Running AOT compiled');
+
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
