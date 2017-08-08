cae02e88ef040b08eb2a99b98fa208c976f638a2
max <max@max.max>
2017-08-07 19:39:09 -0500

    added first compponent

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by lima on 8/7/17.
+ */
+
+import {Component} from '@angular/core'
+
+@Component({
+    selector : 'my-app',
+    template: `<div> {{title}} </div>
+    `
+
+})
+export class AppComponent {
+    title = "Name1"
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 /**
- * Created by pivotal on 8/7/17.
  */
 
-import  {NgModule} from '@angular/core';
+import  {NgModule}      from '@angular/core';
+import  {BrowserModule} from '@angular/platform-browser'
+
+import {AppComponent}   from "./app.component";
 
 
 @NgModule({
-    imports: [NgModule],
-    declarations: [AppModule],
-    bootstrap: [AppModule]
+    imports: [
+        BrowserModule
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
